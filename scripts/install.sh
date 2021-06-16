64d346d6187122d16181936392b0e25e280cd2f4
Marcel <marcel.bernet@mc-b.ch>
2021-06-16 19:14:54 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,7 +15,12 @@ sudo systemctl restart apache2
 bash -x /opt/lernmaas/helper/intro
 
 # PHP Startseite
-
 cat <<%EOF% >~/data/index.php
  <?php echo '<p>In diesem Verzeichnis koennen eigene PHP Dateien abgelegt werden.</p>'; ?>
+%EOF%
+
+# Konfiguration von MySQL
+cat <<%EOF% | sudo mysql 
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY "$(cat ~/data/.ssh/passwd)";
+FLUSH PRIVILEGES;
 %EOF%
