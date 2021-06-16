34d57a4578bb3314b2ef72592f5a6acb54a1ab52
Marcel <marcel.bernet@mc-b.ch>
2021-06-16 17:42:39 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,3 +7,6 @@
 sudo apt-get install -y apache2 curl wget mysql-server php libapache2-mod-php php-mysql adminer
 
 sudo systemctl restart apache2
+
+# Introseite 
+bash -x /opt/lernmaas/helper/intro
